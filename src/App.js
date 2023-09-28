5faca0cd04ce4b7a0b34c1fc6f58c0e241a78e9a
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-09-28 15:00:14 +0300

    its working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="desktop">
+            <div className="div">
+                <header className="header">
+                    <img className="vector" alt="Vector" img="instagram.png" />
+                    <div className="search">
+                        <div className="overlap-group">
+                            <input className="input" placeholder="Search..." type="text" />
+                            <img className="iwwa-search" alt="Iwwa search" img="search.png" />
+                        </div>
+                    </div>
+                    <div className="friends-box">FRIENDS BOX</div>
+                    <div className="navbar">
+                        <div className="text-wrapper">Home</div>
+                        <div className="text-wrapper-2">Shop all</div>
+                        <div className="text-wrapper-3">Our story</div>
+                        <div className="text-wrapper-4">Our craft</div>
+                        <div className="text-wrapper-5">Contact</div>
+                    </div>
+                    <div className="group">
+                        <div className="text-wrapper-6">Log in</div>
+                        <div className="text-wrapper-7">Cart (0)</div>
+                    </div>
+                </header>
+                <div className="text-wrapper-8">Slogan</div>
+                <div className="shpo-the-collection">
+                    <div className="overlap">
+                        <div className="text-wrapper-9">Shop the collection</div>
+                    </div>
+                </div>
+            </div>
+        </div>
   );
 }
 
