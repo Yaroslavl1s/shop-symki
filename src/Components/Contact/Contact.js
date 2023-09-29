bd0e281ccabcec9687492f4ddd7a962f114db468
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-09-29 15:20:05 +0300

    Начало работы

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,17 @@
+import './Contact.css';
+
+
+function Contact() {
+    return (
+        <div className='contact'>
+            <div className='slogan'>
+                <h1>Get in Touch</h1>
+            </div>
+            <div className='service'>
+                <h2>Customer Service</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Contact;
