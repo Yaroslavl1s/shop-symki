252e3ae9f90e20c92bbba9ee93a83470f3690f96
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-10-03 19:25:48 +0300

    button home

--- src/Components/Header/components/FB/FB.js
+++ src/Components/Header/components/FB/FB.js
@@ -10,11 +10,11 @@ function FB() {
             <header className="headerFB">
             <div className="friends-box">FRIENDS BOX</div>
                     <div className="navbar">
-                        <div className="text-wrapper">Home</div>
-                        <div className="text-wrapper-2">Shop all</div>
-                        <div className="text-wrapper-3">Our story</div>
-                        <div className="text-wrapper-4">Our craft</div>
-                        <div className="text-wrapper-5">Contact</div>
+                        <div className="text-wrapper"><button href="#" className="Bhome">Home</button></div>
+                        <div className="text-wrapper-2"><button href="#" className="BshopA">Shop all</button></div>
+                        <div className="text-wrapper-3"><button href="#" className="BourS">Our story</button></div>
+                        <div className="text-wrapper-4"><button href="#" className="BourC">Our craft</button></div>
+                        <div className="text-wrapper-5"><button href="#" className="Contact">Contact</button></div>
                     </div>
             </header>
          </div>
