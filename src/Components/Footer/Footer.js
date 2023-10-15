dd17f4844c2a70a5aeb159985977495450454191
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-10-15 16:18:04 +0300

    almost finished with the footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,14 +3,60 @@ import './Footer.sass';
 
 function Footer() {
     return (
-        <footer className='footer'>
-            <div className='footer_row'>
-                <div className='footer_logo'>
-                    <h2>Logo</h2>
-                    <p>© 2023  All rights reserved</p>
+        <div>
+            <footer className='footer'>
+                <div className='footer__row'>
+                    <div className='footer__logo'>
+                        <h2>Logo</h2>
+                        <p>© 2023  All rights reserved</p>
+                    </div>
+                    <div className='footer__nav'>
+                        <h4>Menu —</h4>
+            	        <div className="footer__nav__menu">
+                            <ul className="footer__nav__pages">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Shop All</a></li>
+                                <li><a href="#">Our Story</a></li>
+                                <li><a href="#">Our Craft</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div className='footer__nav__inf'>
+                            <ul className="footer__nav__list">
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">Shipping & Returns</a></li>
+                                <li><a href="#">Store Policy</a></li>
+                                <li><a href="#">Paymant Methods</a></li>
+                                <li><a href="#">Stockists</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='footer__social'>
+                        <h4>Follow Us</h4>
+                        <ul>
+                            <li><a href='#'>Instagram</a></li>
+                        </ul>
+                    </div>
+                    <div className='footer__mail'>
+                        <h4>Join Us!</h4>
+                        <p>Email*</p>
+                        <p>
+                            <div className='footer__input'>
+                                <input type="text" class="form__control" />
+                                <span className='footer__bttn'>
+                                    <button className='footer__btn' type='button'>
+                                        <span className='footer__send'></span>
+                                    </button>
+                                </span>
+                            </div>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </footer>
+            </footer>
+            <section>
+                <p>Designed by <a href="http://enfoplus.net">Prince J. Sargbah</a></p>
+            </section>
+        </div>
     );
 }
 
