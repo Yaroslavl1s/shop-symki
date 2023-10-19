01a6447e691ee5607f6697ac02d7e6c75ddcb3a0
Max <fedukhinm@mail.ru>
2023-10-19 17:15:08 +0300

    structurised

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  
+ export const productData =[
+  {
+    id: 1,
+    imageurl: "https://millzkarta.ru/blog/wp-content/uploads/2022/10/tout.jpg",
+    name: "Cумка",
+    price: "$20.99",
+    description: "Крутая сумка",
+  },
+  {
+    id: 2,
+    imageurl: "https://millzkarta.ru/blog/wp-content/uploads/2022/10/tout.jpg",
+    name: "Cумка 2",
+    price: "$40.99",
+    description: "Крутая сумка 2",
+  }
+
+
+  ]
