65b9706b43f5f84a582653dc52f772a7df60a69b
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-10-19 19:30:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,61 @@ import Button from './Components/Header/components/Button/Button';
 import FB from './Components/Header/components/FB/FB';
 import Home from './Components/Header/components/Home/Home';
 import OurC from './Components/OurC/OurC';
-
+import Carousel from "react-elastic-carousel";
+import Product from './Product.jsx'
+import 'react-multi-carousel/lib/styles.css';
 
 
 
 
 
 function App() {
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  
+  const productData =[
+  {
+    id: 1,
+    imageurl: "https://millzkarta.ru/blog/wp-content/uploads/2022/10/tout.jpg",
+    name: "Cумка",
+    price: "$20.99",
+    description: "Крутая сумка",
+  },
+  {
+    id: 2,
+    imageurl: "https://millzkarta.ru/blog/wp-content/uploads/2022/10/tout.jpg",
+    name: "Cумка 2",
+    price: "$40.99",
+    description: "Крутая сумка 2",
+  }
+
+
+  ]
+  
+  const product = productData.map(item => (
+
+    <Product name={item.name} url={item.imageurl} price={item.price} description={item.description} />
+  ))
+
+
   return (
     <>
     <Contact/>
