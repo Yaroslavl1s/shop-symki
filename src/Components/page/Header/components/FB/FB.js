8b8940dd37d1078e94e2b5d17bdacbcea8c1ce93
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-10-26 19:54:40 +0300

    header , button LC

--- src/Components/page/Header/components/FB/FB.js
+++ src/Components/page/Header/components/FB/FB.js
@@ -1,27 +1,30 @@
 import './FB.sass';
 import { Link} from 'react-router-dom'
-
+import instagramblack from './../../../../../image/instagramblack.png'
 
 
 export const FB = () => {
     return (
         <header className="headerFB">
             <div className="headerFB__friends-box">FRIENDS BOX</div>
+
+            <img className="instagramblack" alt="Vector" src={instagramblack} />
+            
                 <div className="headerFB__navbar">
                     <Link to="home">
-                        <a href='src/components/page/Home'>Home</a>
+                        <a className="headerFB__navbar__Btn"href='src/components/page/Home'>Home</a>
                     </Link>
                     <Link to="shop all">
-                        <a href='src/components/page/Shop'>Shop all</a>
+                        <a className="headerFB__navbar__Btn"href='src/components/page/Shop'>Shop all</a>
                     </Link>
                     <Link to="our story">
-                        <a href='src/components/page/OurS'>Our Story</a>
+                        <a className="headerFB__navbar__Btn"href='src/components/page/OurS'>Our Story</a>
                     </Link>
                     <Link to="our craft">
-                        <a href='src/components/page/OurC'>Our Craft</a>
+                        <a className="headerFB__navbar__Btn"href='src/components/page/OurC'>Our Craft</a>
                     </Link>
                     <Link to="contact">
-                        <a href="src/components/page/index">Contact</a>
+                        <a className="headerFB__navbar__Btn"href="src/components/page/index">Contact</a>
                     </Link>
                 </div>
         </header>
