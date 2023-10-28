6a6549b5f774700bee79547a8f6d09fd240b7d91
Max <fedukhinm@mail.ru>
2023-10-28 18:51:42 +0300

    ya vse isportil

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,13 @@ export const Home = () => {
     return (
      <div className="Selars">
           <h4>BEST SELLERS</h4>
+          <>
           
+          <div id="root"></div>
+
+          
+          
+          </>
 
           <button className="btmALL"> Shop All Bags</button>
      </div>
