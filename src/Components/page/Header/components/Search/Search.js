9dfc834e0e6b54b9d264acf5b60eb5fcac139c57
Navi321 <49982747+Navi321@users.noreply.github.com>
2023-10-29 19:55:42 +0200

    attempt to improve the search engine

--- src/Components/page/Header/components/Search/Search.js
+++ src/Components/page/Header/components/Search/Search.js
@@ -1,15 +1,12 @@
 import './Search.sass';
-import { Link} from 'react-router-dom'
 import searchblack from'../../../../../image/searchblack.png'
 
 export const Search = () => {
     return (
-        <div>
-            <div className="search">
-                    <div className="overlap-group">
-                        <input className="input" placeholder="Search..." type="text" />
-                        <img className="iwwa-search" alt="Iwwa search" src={searchblack} />
-                    </div>
+        <div className='search'>
+            <div className="search__window">
+                <input className="search__input" placeholder="Search..." type="text" />
+                <img className="search__img" alt="Iwwa search" src={searchblack} />
             </div>
         </div>
     )
