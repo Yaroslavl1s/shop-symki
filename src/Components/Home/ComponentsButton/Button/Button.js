c22b945a69123265cc8204343389627c42b9500e
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-11-05 19:32:35 +0300

    pomogite

--- src/Components/Home/ComponentsButton/Button/Button.js
+++ src/Components/Home/ComponentsButton/Button/Button.js
@@ -7,7 +7,7 @@ export const Button = () => {
     return(
         <div className="shpo-the-collection">
             <Link to="shop-all">
-                <a className="ButtonShop"href='src/components/page/Shop'>Shop the collection</a>
+                <a className="ButtonShop"href='src/components/Home/ComponentsButton/Shop'>Shop the collection</a>
             </Link>
         </div>
     )
