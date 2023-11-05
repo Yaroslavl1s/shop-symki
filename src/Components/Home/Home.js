52a8518f184ec3f7761437a91c4fbd1ba6c017ac
Max <fedukhinm@mail.ru>
2023-11-05 21:23:49 +0300

    buttons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,13 +7,14 @@ import Carousel from './Slider/Carousel'
 export const Home = () => {
     return (
      <div className="Selars">
+          <h1>Most Popular</h1>
           
           <>
           <Carousel/>
           </>
           
           
-        
+          <button class="button">Shop All</button>
           
      </div>
     
